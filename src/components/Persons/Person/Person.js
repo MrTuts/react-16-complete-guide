896b3ad3189a817b57320a011f5568cde9057c7c
MrTuts <pavelgric+tuts@gmail.com>
2019-01-11 14:23:50 +0100

    transform Person and Persons to statefull components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        Hola {props.name}, tú tienes {props.age} anos
-      </p>
-      {props.children && <p>{props.children}</p>}
-      <input type="text" onChange={props.change} value={props.name} />
-    </div>
-  );
-};
+class person extends Component {
+  render() {
+    const props = this.props;
+    return (
+      <div className={classes.Person}>
+        <p onClick={props.click}>
+          Hola {props.name}, tú tienes {props.age} anos
+        </p>
+        {props.children && <p>{props.children}</p>}
+        <input type="text" onChange={props.change} value={props.name} />
+      </div>
+    );
+  }
+}
 
 export default person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = props =>
-  props.persons.map(({ name, age, id }, index) => (
-    //will only work in production build
-    <Person
-      name={name}
-      age={age}
-      click={() => props.clicked(index)}
-      change={event => props.changed(event, id)}
-    />
-  ));
+class Persons extends Component {
+  render() {
+    return this.props.persons.map(({ name, age, id }, index) => (
+      //will only work in production build
+      <Person
+        name={name}
+        age={age}
+        click={() => this.props.clicked(index)}
+        change={event => this.props.changed(event, id)}
+      />
+    ));
+  }
+}
 
 export default Persons;
